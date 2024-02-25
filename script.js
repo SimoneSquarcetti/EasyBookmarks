b5f1d9bacf5c63be07d980913c27bff06b6abc94
SimoneSquarcetti <161199585+SimoneSquarcetti@users.noreply.github.com>
2024-02-25 10:23:21 +0100

    added newCategory function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('C:/Users/simos/Desktop/EasyBookmarks/categories.json', {
+/*fetch('C:/Users/simos/Desktop/EasyBookmarks/categories.json', {
     method: 'GET',
     headers: {
     'Access-Control-Allow-Origin': '*',
@@ -7,5 +7,14 @@ fetch('C:/Users/simos/Desktop/EasyBookmarks/categories.json', {
     .then((json) => console.log(json))
     .catch(err => console.error(err));
 
+*/
+function addCategory(name){
+   var newCategoryDiv = document.createElement("div");
+   newCategoryDiv.classList.add("categoryDiv");
 
+   newCategoryDiv.textContent = name;
+   var divEsistente = document.getElementById("contentDiv").getElementsByTagName("div")[0];
 
+   document.getElementById("contentDiv").insertBefore(newCategoryDiv, divEsistente);
+
+}
--- server.js
+++ server.js
@@ -3,3 +3,4 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
  
+
